d2733f5a66aca3bcf0bf50689a09cb56d0ca4fcf
Saikat Das <saikatdas0790@gmail.com>
2025-07-22 14:05:23 +0000

    Refactor List struct to be generic over type T, updating push and pop methods, and Node struct accordingly

--- src/second.rs
+++ src/second.rs
@@ -1,15 +1,15 @@
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-pub struct List {
-    head: Link,
+pub struct List<T> {
+    head: Link<T>,
 }
 
-impl List {
+impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
     }
 
-    pub fn push(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem,
             next: self.head.take(),
@@ -18,7 +18,7 @@ impl List {
         self.head = Some(new_node);
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|boxed_node| {
             self.head = boxed_node.next;
             boxed_node.elem
@@ -26,13 +26,13 @@ impl List {
     }
 }
 
-impl Default for List {
+impl<T> Default for List<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl Drop for List {
+impl<T> Drop for List<T> {
     fn drop(&mut self) {
         let mut cur_link = self.head.take();
         while let Some(mut boxed_node) = cur_link {
@@ -42,9 +42,9 @@ impl Drop for List {
     }
 }
 
-struct Node {
-    elem: i32,
-    next: Link,
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
 }
 
 #[cfg(test)]
@@ -53,7 +53,7 @@ mod tests {
 
     #[test]
     fn test_push_pop() {
-        let mut list = List::new();
+        let mut list: List<i32> = List::new();
 
         assert_eq!(list.pop(), None);
 
