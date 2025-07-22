46e9835ccc90c67f8c96fa0025f8fdb31e93253f
Saikat Das <saikatdas0790@gmail.com>
2025-07-22 14:01:16 +0000

    Implement List struct with push and pop methods, Default and Drop traits, and add unit tests

---
+++ src/second.rs
@@ -0,0 +1,76 @@
+type Link = Option<Box<Node>>;
+
+pub struct List {
+    head: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem,
+            next: self.head.take(),
+        });
+
+        self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        self.head.take().map(|boxed_node| {
+            self.head = boxed_node.next;
+            boxed_node.elem
+        })
+    }
+}
+
+impl Default for List {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = self.head.take();
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = boxed_node.next.take();
+            // boxed_node will be dropped here, freeing the memory
+        }
+    }
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_push_pop() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
