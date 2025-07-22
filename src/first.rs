71092a3553b944e07f194e89d61deffaf5b1f476
Saikat Das <saikatdas0790@gmail.com>
2025-07-22 04:26:28 +0000

    Add unit tests for push and pop methods in List struct

--- src/first.rs
+++ src/first.rs
@@ -29,6 +29,34 @@ impl List {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_push_pop() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
+
 enum Link {
     Empty,
     More(Box<Node>),
