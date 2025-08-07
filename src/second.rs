1b39ef67685bda030d53a8876abd775b249a39fa
Saikat Das <saikatdas0790@gmail.com>
2025-08-07 14:24:43 +0000

    Implement iterator for List struct with into_iter method and corresponding tests

--- src/second.rs
+++ src/second.rs
@@ -32,6 +32,10 @@ impl<T> List<T> {
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map(|node| &mut node.elem)
     }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
 }
 
 impl<T> Default for List<T> {
@@ -47,6 +51,16 @@ impl<T> Drop for List<T> {
             cur_link = boxed_node.next.take();
             // boxed_node will be dropped here, freeing the memory
         }
+    }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
     }
 }
 
@@ -101,5 +115,20 @@ mod tests {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list: List<i32> = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
