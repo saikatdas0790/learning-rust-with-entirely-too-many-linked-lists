3e10b208ec7d853f4c7a189c1e3a4aaf24159606
Saikat Das <saikatdas0790@gmail.com>
2025-08-07 14:12:11 +0000

    Add peek and peek_mut methods to List struct with corresponding tests

--- src/second.rs
+++ src/second.rs
@@ -24,6 +24,14 @@ impl<T> List<T> {
             boxed_node.elem
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
 }
 
 impl<T> Default for List<T> {
@@ -72,5 +80,26 @@ mod tests {
 
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list: List<i32> = List::new();
+
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        if let Some(value) = list.peek_mut() {
+            *value = 42;
+        }
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
     }
 }
